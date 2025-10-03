71a42e8d401841502b74f673fd5cb985dd019f9a
Sot. Tzelatidis <stzelati@gmail.com>
2025-10-03 21:09:27 +0300

    feat: custom hook useAuth added

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,8 @@
+import { useContext } from "react";
+import { AuthContext} from "@/context/AuthContext.ts";
+
+export function useAuth() {
+  const authContext = useContext(AuthContext);
+  if (!authContext) throw new Error("useAuth must be used within AuthProvider");
+  return authContext;
+}
